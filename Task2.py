ee70ecd499b3fd9e1a5f3aa3a7aa0d27318d4929
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-04-28 19:22:32 -0700

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -9,3 +9,20 @@ for numb in fibo_list:
     if numb % 2 == 0:
         c = c + numb
 print(c)
+
+
+#Here is an other way to solve this
+
+fib_seq, fib_seq2 = 1, 1 
+sum_feb = 0
+
+# Here we make sure that our fib number is bellow 4 million.
+while fib_seq <= 4e6:
+    
+    fib_seq, fib_seq2 = fib_seq2, fib_seq + fib_seq2 
+    
+    #Here we make sure that we add only even values to our SUM
+    
+    if fib_seq % 2 == 0:
+        sum_feb = sum_feb + fib_seq
+print(sum_feb)
